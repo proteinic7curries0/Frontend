d961ecf2c801dd19235155e4d9bf9de5d83c44f8
proteinic7curries0 <24hnnzjrkp@privaterelay.appleid.com>
2026-09-20 15:03:31 +0300

    feat: remove genomes

--- src/sidebar/RunTab.tsx
+++ src/sidebar/RunTab.tsx
@@ -9,14 +9,6 @@ import { GenomeLink } from "./dialogs/genome";
 import { CraftingLink, type Edge, type Node } from "./dialogs/crafting";
 
 export default function RunTab({ seed, onSeedChange, agentsCount, setAgentsCount, onStart, isRunning, nodes, setNodes, edges, setEdges  }: { seed: string, onSeedChange: (seed: string) => void, agentsCount: string, setAgentsCount: (count: string) => void, onStart: () => void, isRunning: boolean, nodes: Node[], setNodes: React.Dispatch<React.SetStateAction<Node[]>>, edges: Edge[], setEdges: React.Dispatch<React.SetStateAction<Edge[]>> }) {
-    const genomes = useMemo(() => {
-        let genomes = []
-        for (let i = 0; i < 10; i++) {
-            genomes.push(new Genome(i));
-        }
-        return genomes;
-    }, [])
-
     return (
         <Tabs.Panel className={panelClassName} value="run">
             <div className="flex gap-5">
@@ -29,7 +21,6 @@ export default function RunTab({ seed, onSeedChange, agentsCount, setAgentsCount
                 </div>
                 <div>
                     <h1 className="text-sm text-neutral-950 dark:text-white">Extras</h1>
-                    <GenomeLink genomes={genomes} /><br /> {/* Dialog.Root makes them inline XXD */}
                     <CraftingLink nodes={nodes} setNodes={setNodes} edges={edges} setEdges={setEdges} />
                 </div>
             </div>
